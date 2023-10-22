9cc2342d2115b12977f678371e58084debddb0f5
Dushess0 <ilyabannila@gmail.com>
2023-10-22 14:07:51 +0200

    added basic serwer setuo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,40 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import { World } from 'miniplex';
+import { playerEntity } from './types';
+import { vec3 } from 'gl-matrix';
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+const world = new World<playerEntity>();
+const TICK_RATE = 1000 / 60; // 60 FPS
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express and TypeScript!');
+io.on('connection', (socket) => {
+	console.log('a user connected', socket.id);
+
+	const playerEntity: playerEntity = {
+		position: vec3.fromValues(0, 0, 0),
+		velocity: vec3.fromValues(0, 0, 0),
+		playerId: socket.id
+	};
+	world.add(playerEntity);
+	socket.on('disconnect', () => {
+		console.log('user disconnected', socket.id);
+		world.where((p) => p.playerId == socket.id);
+	});
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+function movementSystem() {
+	const movingEntities = world.with('position', 'velocity');
+	for (const entity of movingEntities) {
+		vec3.add(entity.position, entity.position, entity.velocity);
+	}
+}
+
+setInterval(() => {
+	movementSystem();
+	const state = world.entities;
+	io.emit('gameUpdate', state);
+}, TICK_RATE);
